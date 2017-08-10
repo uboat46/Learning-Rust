ab28cb70c61a0a59a27950a1b381b95cd90ee5aa
uboat46 <uboat46@hotmail.com>
2017-08-10 02:50:47 -0500

    finished loops!

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,3 +1,37 @@
+// // Infinite Loop
+// fn main() {
+//     loop {
+//         println!("again!");
+//     }
+// }
+
+// // While Loop
+// fn main() {
+//     let mut number = 3;
+
+//     while number != 0 {
+//         println!("{}!", number);
+
+//         number = number - 1;
+//     }
+    
+//     println!("LIFT OFF!!!");
+// }
+
+// // For Loop
+// fn main() {
+//     let a = [10, 20, 30, 40, 50];
+
+//     for element in a.iter() {
+//         println!("The value is: {}", element);
+//     }
+// }
+
+// Nicier For Loop
 fn main() {
-    println!("Hello, world!");
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    
+    println!("LIFT OFF!!!");
 }
---
+++ loops/target/debug/loops.d
@@ -0,0 +1,1 @@
+/Users/uboat46/Desktop/Learning-Rust/loops/target/debug/loops: /Users/uboat46/Desktop/Learning-Rust/loops/src/main.rs
